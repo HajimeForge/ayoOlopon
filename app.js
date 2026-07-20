e10fe943cbec1ec0103a95a1057b3ae32b521cec
rayhaantechcare2020 <rayhaantechcare2020.com>
2026-07-20 19:07:58 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+app.use(express.json());
+
+//Create Function
+
+
+
+
+
+
+//Get All
+
+
+
+
+
+
+//Get on by Id and phone
+
+
+
+
+
+
+
+
+
+//Update
+
+
+
+
+
+//Delete
