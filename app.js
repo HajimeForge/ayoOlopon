adc087cac1658d3c0b689bd099c7b69eeca5648c
Your Name <danoscolobo@gmail.com>
2026-07-21 21:17:03 +0100

    Added create player endpoint

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
+const players = [];
+
 //Create Function
+app.post("/createplayers", (req, res) => {
+  const { name } = req.body;
+
+  const newPlayer = {
+    id: players.length + 1,
+    name,
+    score: 0,
+    capturedSeeds: 0,
+  };
+
+  players.push(newPlayer);
+
+  res.status(201).json({
+    message: "Player created successfully",
+    player: newPlayer,
+  });
+});
 
 
 
@@ -35,3 +54,7 @@ app.use(express.json());
 
 
 //Delete
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
